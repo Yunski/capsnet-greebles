fix smalnorb test normalization and testing queue

capsnet.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
-from utils import get_train_batch
+from utils import get_train_batch, get_test_batch
 from config import cfg
 
 class CapsNet(object):
@@ -21,7 +21,7 @@ class CapsNet(object):
                 self.train_op = self.optimizer.minimize(self.total_loss, global_step=self.global_step)
             else:
                 if use_test_queue:
-                    self.X, self.labels = get_test_batch(cfg.dataset, cfg.batch_size, cfg.num_threads, samples_per_epoch=cfg.samples_per_epoch)
+                    self.X, self.labels = get_test_batch(cfg.dataset, cfg.test_batch_size, cfg.num_threads)
                     self.Y = tf.one_hot(self.labels, depth=10, axis=1, dtype=tf.float32)
                 else:
                     self.X = tf.placeholder(tf.float32, shape=self.input_shape)

cnn_baseline.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from config import cfg
-from utils import get_train_batch, variable_on_cpu
+from utils import get_train_batch, get_test_batch, variable_on_cpu
 
 class CNN(object):
     def __init__(self, input_shape, is_training=True, use_test_queue=False):
@@ -19,7 +19,7 @@ class CNN(object):
                 self.train_op = self.optimizer.minimize(self.total_loss, global_step=self.global_step)
             else:
                 if use_test_queue:
-                    self.X, self.labels = get_test_batch(cfg.dataset, cfg.batch_size, cfg.num_threads, samples_per_epoch=cfg.samples_per_epoch)
+                    self.X, self.labels = get_test_batch(cfg.dataset, cfg.test_batch_size, cfg.num_threads)
                 else:
                     self.X = tf.placeholder(tf.float32, shape=self.input_shape)
                     self.labels = tf.placeholder(tf.int32, shape=(self.input_shape[0],))

smallnorb.py
@@ -3,8 +3,6 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-from config import cfg
-
 np.random.seed(10234)
 
 def plot_imgs(inputs, labels):
@@ -92,7 +90,7 @@ def read_norb_tfrecord(filenames):
 
     assert isinstance(filenames, list)
 
-    filename_queue = tf.train.string_input_producer(filenames, num_epochs=cfg.epochs)
+    filename_queue = tf.train.string_input_producer(filenames, num_epochs=None)
     reader = tf.TFRecordReader()
     _, serialized_example = reader.read(filename_queue)
     features = tf.parse_single_example(serialized_example,
@@ -110,11 +108,11 @@ def load_norb(batch_size, samples_per_epoch=None, is_training=True):
     if is_training:
         num_train_batches = 24300 * 2 // batch_size
         # do not provide training or validation data here
-        return None, None, None, None, num_train_batches, 0
+        return [], [], [], [], num_train_batches, 0
     else:
         num_test_batches = 24300 * 2 // batch_size
         # do not provide test data here
-        return None, None, num_test_batches
+        return [], [], num_test_batches
 
 
 if __name__ == '__main__':

utils.py
@@ -61,13 +61,23 @@ def get_test_batch(dataset, batch_size, num_threads, min_after_dequeue=5000, sam
         CHUNK_RE = re.compile(r"test-\d+\.tfrecords")
         data_dir = "data/smallnorb"
         chunk_files = [os.path.join(data_dir, fname) for fname in os.listdir(data_dir) if CHUNK_RE.match(fname)]
-        X_test, Y_test = read_norb_tfrecord(chunk_files, epochs)
+        X_test, Y_test = read_norb_tfrecord(chunk_files)
         X_test = tf.image.resize_images(X_test, [48, 48])
         X_test = tf.slice(X_test, [8, 8, 0], [32, 32, 1])
+        params_shape = [X_test.get_shape()[-1]]
+        beta = tf.get_variable(
+            'beta', params_shape, tf.float32,
+            initializer=tf.constant_initializer(0.0, tf.float32))
+        gamma = tf.get_variable(
+            'gamma', params_shape, tf.float32,
+            initializer=tf.constant_initializer(1.0, tf.float32))
+        mean, variance = tf.nn.moments(X_test, [0, 1, 2])
+        X_test = tf.nn.batch_normalization(X_test, mean, variance, beta, gamma, 0.001)
+        data_queues = [X_test, Y_test]
     else:
         raise ValueError("{} is not an available dataset".format(dataset))
 
-    X, Y = tf.train.shuffle_batch([X_test, Y_test], num_threads=num_threads,
+    X, Y = tf.train.shuffle_batch(data_queues, num_threads=num_threads,
                                   batch_size=batch_size,
                                   capacity=min_after_dequeue + (num_threads + 1) * batch_size,
                                   min_after_dequeue=min_after_dequeue,