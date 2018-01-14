3eaac09c24de356cfc5a701a3c63d4508d927dde
Xiao Yang Yu <xyyxiaoyangyu@gmail.com>
2018-01-13 23:39:21 -0500

    yay

--- resnet.py
+++ resnet.py
@@ -36,7 +36,7 @@ class resnet(object):
             filter_size = 7
             stride = 1
             num_filters = 64
-            conv_0 = conv_layer(inputs, filter_size, filter_size, inputs.shape[-1], num_filters, stride)
+            conv_0 = self.conv_layer(inputs, filter_size, filter_size, inputs.shape[-1], num_filters, stride)
             activation_0 = tf.nn.relu(conv_0, name=scope.name)
         with tf.variable_scope('pool0') as scope:
             pool_0 = tf.nn.max_pool(activation_0, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
@@ -49,7 +49,7 @@ class resnet(object):
         num_filters = 64
         for i in range(num_layers):
             with tf.variable_scope('conv%d' % (i+1)) as scope:
-                res = res_layer(nodes[-1], num_filters)
+                res = self.res_layer(nodes[-1], num_filters)
                 nodes.append(res)
 
         # End of residual layers.
@@ -59,7 +59,7 @@ class resnet(object):
 
         # Finally, fully-connected layer which outputs the logits for classification.
         num_classes = 10
-        logits = dense_layer(avg_pool, num_filters, num_classes)
+        logits = self.dense_layer(avg_pool, num_filters, num_classes)
         
         self.logits = logits
 
@@ -96,12 +96,12 @@ class resnet(object):
         stride = 1
 
         # Convolutional layer 1.
-        conv_1 = conv_layer(x, filter_size, filter_size, x_depth, num_filters, stride)
+        conv_1 = self.conv_layer(x, filter_size, filter_size, x_depth, num_filters, stride)
         # ReLU activation.
         activation_1 = tf.nn.relu(conv_1)
 
         # Convolutional layer 2.
-        conv_2 = conv_layer(activation_1, filter_size, filter_size, num_filters, num_filters, stride)
+        conv_2 = self.conv_layer(activation_1, filter_size, filter_size, num_filters, num_filters, stride)
         # Add initial input through shortcut connection (defined as F + x in the paper) before activation.
         res = tf.add(conv_2, x)
         activation_2 = tf.nn.relu(res)
