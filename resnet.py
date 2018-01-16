1a278f0dfd98e5bec8090b8dd959bf7bb22b3ed6
Xiao Yang Yu <xyyxiaoyangyu@gmail.com>
2018-01-15 21:43:11 -0500

    final resnet

--- resnet.py
+++ resnet.py
@@ -70,12 +70,12 @@ class resnet(object):
             nodes.append(pool_2)
 
         # Set of residual layers #3.
-        # num_layers = 12
-        # num_filters = 256
-        # for i in range(num_layers):
-        #     with tf.variable_scope('res3_%d' % (i)) as scope:
-        #         res = self.res_layer(nodes[-1], num_filters)
-        #         nodes.append(res)
+        num_layers = 12
+        num_filters = 256
+        for i in range(num_layers):
+            with tf.variable_scope('res3_%d' % (i)) as scope:
+                res = self.res_layer(nodes[-1], num_filters)
+                nodes.append(res)
 
         # End of residual layers. As per the paper, no maxpooling or dropout occurs at the end of the final convolution.
 
